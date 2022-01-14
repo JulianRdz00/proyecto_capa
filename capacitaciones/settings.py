c39e2a1ecda35c650477ebc33675e64b426b5187
Alberto Agustin Taborda <albertoagustintaborda@gmail.com>
2022-01-14 15:59:36 -0300

    modificacion settings, y modelos

--- capacitaciones/settings.py
+++ capacitaciones/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't$4n_4i8r^$ge%5g+1ke6ltc4l^&=3o34)f7b04#zf1tsebop3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost']
 
 
 # Application definition
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-AR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Buenos_Aires'
 
 USE_I18N = True
 
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- perfiles/models.py
+++ perfiles/models.py
@@ -1,3 +1,34 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Perfiles(models.Model):
+    nombres = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=20)
+    email = models.EmailField(max_length=80)
+	# pais = models.TextField(max_length=50) (fijarse si hay alguna forma de seleccionar pais)
+    # genero = models.TextField(max_length=50) (fijarse si hay alguna forma de configurar opciones a seleccionar)
+    # conocimientos_ingles = models.TextField(max_length=50) (fijarse si hay alguna forma de configurar opciones a seleccionar)
+    formacion_academica = models.TextField(max_length=500)
+    profesion = models.TextField(max_length=100)
+    fotos = models.ImageField(upload_to="fotos", null=True)
+    webPage = models.CharField(max_length=100)
+    twitter = models.CharField(max_length=100)
+    facebook = models.CharField(max_length=100)
+    linkedin = models.CharField(max_length=100)
+    youtube = models.CharField(max_length=100)
+    biografia = models.CharField(max_length=500)
+    nacimiento = models.DateField(
+		default=timezone.now, null = True)
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
