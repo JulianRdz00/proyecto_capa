3226830040a89ee342a2614e108078297d9c3aa6
Alberto Agustin Taborda <albertoagustintaborda@gmail.com>
2022-02-01 21:57:55 -0300

    se solucionan errores

--- libs/models.py
+++ libs/models.py
@@ -1,11 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 
 class CommonInfo(models.Model):
     fecha_de_creación = models.DateTimeField(default=timezone.now)
     fecha_de_publicación = models.DateTimeField(blank=True, null=True)
-    is_deleted = models.BoleanField(default=False)
-
+    is_deleted = models.BooleanField(default=False)
 
     class Meta:
         abstract = True
